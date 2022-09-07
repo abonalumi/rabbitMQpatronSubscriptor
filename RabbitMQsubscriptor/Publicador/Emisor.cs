54a0c935c762431c8e2369b2213b55b4e3e37c06
abonalumi <augustobonalumi@hotmail.com>
2022-09-06 22:58:17 -0300

    RabbitMQ con Exchange
    Se incluye el concepto de exchange para trabajar con un publisher y varios subscribers.

---
+++ RabbitMQsubscriptor/Publicador/Emisor.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var fabrica = new ConnectionFactory() { HostName = "localhost" };
+using(var connection = fabrica.CreateConnection())
+//se crea un canal
+using(var canal = connection.CreateModel())
+{
+    // Se remueve la cola y se agrega un Exchange (el exchange puede tener muchas colas y se puede definir varios emisores y receptores)
+    canal.ExchangeDeclare(exchange:"logs", type:ExchangeType.Fanout);
+    // se crea un mensaje
+    string mensaje = GetMessage(args);
+    // se codifica el mensaje a bytes
+    var cuerpo = Encoding.UTF8.GetBytes(mensaje);
+    // se envía el mensaje
+    canal.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: cuerpo);
+    // visualizar el mensaje enviado en consola para ver si la línea de ejecución llegó hasta este punto y qué se envió.
+    Console.WriteLine("[x] Enviado {0}", mensaje);
+}
+
+Console.WriteLine("Precionar [enter] para salir.");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "info: Hola mundo");
+}
+
---
+++ RabbitMQsubscriptor/Subscriptor/Receptor.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Crear una fábrica de conexión para crear la conexión del cliente con el servidor según explica
+// la documentación
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using(var connection = factory.CreateConnection())
+using(var canal = connection.CreateModel())
+{
+	canal.ExchangeDeclare(exchange:"logs", type:ExchangeType.Fanout);
+	// el receptor puede tener varias colas
+	var queueName = canal.QueueDeclare().QueueName;
+	// acá se relaciona las colas con el Exchange.
+	canal.QueueBind(queue:queueName, exchange:"logs", routingKey:"");
+
+	Console.WriteLine("Esperando mensajes...");
+
+	// Se crea un consumidor para detectar los mensajes
+	var consumer = new EventingBasicConsumer(canal);
+	consumer.Received += (model, ea) =>
+	{
+		// Si encuentra un mensaje lo mete en un array.
+		var cuerpo = ea.Body.ToArray();
+		// Se codifica los bytes del array a cadena de texto.
+		var mensaje = Encoding.UTF8.GetString(cuerpo);
+		Console.WriteLine(" [x] Recibido {0}", mensaje);
+	};
+
+	// Finalmente se ejecuta la acción en el consumidor que debe mostrar el mensaje que recibió el objeto consumidor.
+	canal.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+	Console.WriteLine("Precionar [enter] para salir.");
+	Console.ReadLine();
+}
